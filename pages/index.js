29054c32cd27cabad89687722ba6b4127fc2eb0f
Andreas <andreas@pop-os.localdomain>
2020-05-17 16:34:19 -0600

    adds first lesson

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Aprendamos NextJS con Webtoriales!</p>
+    </div>
+  )
+}
---
+++ pages/users.js
@@ -0,0 +1,7 @@
+export default function Users() {
+  return(
+    <div>
+      <h1>Users Page</h1>
+    </div>
+  )
+}
