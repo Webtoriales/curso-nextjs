b97a1824ecbb6d62fdd2f8611a3270c4a8fdb3d6
Andreas <andreaslorozco@gmail.com>
2020-06-03 18:17:39 -0600

    adds lesson 6

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,10 +1,12 @@
 import { useRouter } from 'next/router';
+import Navbar from './../../components/navbar';
 
 export default function Post() {
   const router = useRouter();
 
   return (
     <div>
+      <Navbar />
       <h2>Post Details</h2>
       <p>Post ID: {router.query.id}</p>
     </div>
--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,10 +1,12 @@
 import { useRouter } from 'next/router';
+import Navbar from './../../components/navbar';
 
 export default function user1() {
   const router = useRouter();
 
   return (
     <div>
+      <Navbar />
       <h2>User Details</h2>
       <p>User ID: {router.query.id}</p>
     </div>
