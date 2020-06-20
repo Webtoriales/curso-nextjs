0cbc9ef942bc1c55ea39a0126dddb25992832806
Andreas <andreaslorozco@gmail.com>
2020-06-20 14:48:12 -0600

    adds changes to 09

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -9,25 +9,22 @@ export default function Posts() {
     const fetchPosts = async () => {
       const res = await fetch('https://jsonplaceholder.typicode.com/posts');
       const newPosts = await res.json();
-      // console.log("newPosts", newPosts);
       setPosts(newPosts);
-    }
+    };
 
     fetchPosts();
   }, []);
-
-  console.log('posts', posts);
 
   return(
     <Layout>
       <Title>Posts Page</Title>
       <div>
         {posts.map(post => {
-          return(
-            <Link href={`/posts/[id]`} as={`posts/${post.id}`}>
+          return (
+            <Link href={`/posts/[id]`} as={`/posts/${post.id}`} key={post.id}>
               <a>
-                <h3>Post</h3>
-                <p>{post.title}</p>
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
               </a>
             </Link>
           )
