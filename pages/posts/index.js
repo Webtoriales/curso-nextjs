c15b89cbe7ebf691dcc22fd2587cec9faba36dce
Andreas <andreaslorozco@gmail.com>
2020-06-26 14:42:20 -0600

    adds css fr lesson 10

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -18,11 +18,11 @@ export default function Posts() {
   return(
     <Layout>
       <Title>Posts Page</Title>
-      <div>
+      <div className='grid'>
         {posts.map(post => {
           return (
             <Link href={`/posts/[id]`} as={`/posts/${post.id}`} key={post.id}>
-              <a>
+              <a className='card'>
                 <h3>{post.title}</h3>
                 <p>{post.body}</p>
               </a>
@@ -30,6 +30,41 @@ export default function Posts() {
           )
         })}
       </div>
+      <style jsx>
+        {`
+          .grid {
+            display: flex;
+            flex-wrap: wrap;
+            max-width: 800px;
+            margin-top: 2rem;
+          }
+          .card {
+            margin: 1rem;
+            flex-basis: 45%;
+            padding: 1.5rem;
+            color: black;
+            text-decoration: none;
+            border: 2px solid #eaeaea;
+            border-radius: 10px;
+            transition: color 0.15s ease, border-color 0.15s ease;
+          }
+          .card:hover,
+          .card:focus,
+          .card:active {
+            color: #0070f3;
+            border-color: #0070f3;
+          }
+          .card h3 {
+            margin: 0 0 1rem 0;
+            font-size: 1.5rem;
+          }
+          .card p {
+            margin: 0;
+            font-size: 1.25rem;
+            line-height: 1.5;
+          }
+        `}
+      </style>
     </Layout>
   )
 }
