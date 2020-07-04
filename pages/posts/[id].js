acb42c4799fd1fac36dcfbd8593ae2bdb028cc2a
Andreas <andreaslorozco@gmail.com>
2020-07-04 15:37:37 -0600

    adds code from lesson 10

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,14 +1,56 @@
-import { useRouter } from 'next/router';
 import Title from '../../components/title';
 import Layout from '../../components/layout';
 
-export default function Post() {
-  const router = useRouter();
+export default function Post({ post }) {
 
   return (
     <Layout>
       <Title>Post Details</Title>
-      <p>Post ID: {router.query.id}</p>
+      <div className='card'>
+        <h2>{post.title}</h2>
+        <p>{post.body}</p>
+      </div>
+      <style jsx>
+      {`
+        .card {
+          margin: 1rem;
+          flex-basis: 45%;
+          padding: 1.5rem;
+          text-align: left;
+          color: black;
+          text-decoration: none;
+          border: 2px solid #eaeaea;
+          border-radius: 10px;
+          transition: color 0.15s ease, border-color 0.15s ease;
+        }
+        .card:hover,
+        .card:focus,
+        .card:active {
+          color: #0070f3;
+          border-color: #0070f3;
+        }
+        .card h3 {
+          margin: 0 0 1rem 0;
+          font-size: 1.5rem;
+        }
+        .card p {
+          margin: 0;
+          font-size: 1.25rem;
+          line-height: 1.5;
+        }
+      `}
+      </style>
     </Layout>
   )
+}
+
+export async function getServerSideProps({ params }) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`);
+  const post = await res.json();
+
+  return {
+    props: {
+      post
+    }
+  }
 }
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -2,27 +2,28 @@ import Title from '../../components/title';
 import Layout from '../../components/layout';
 import Link from 'next/link';
 
-export default function Posts() {
-  const [posts, setPosts] = React.useState([]);
+export default function Posts({ posts }) {
+  // CLIENT SIDE RENDERING
+  // const [posts, setPosts] = React.useState([]);
 
-  React.useEffect(() => {
-    const fetchPosts = async () => {
-      const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-      const newPosts = await res.json();
-      setPosts(newPosts);
-    };
+  // React.useEffect(() => {
+  //   const fetchPosts = async () => {
+  //     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  //     const newPosts = await res.json();
+  //     setPosts(newPosts);
+  //   };
 
-    fetchPosts();
-  }, []);
+  //   fetchPosts();
+  // }, []);
 
   return(
     <Layout>
       <Title>Posts Page</Title>
-      <div>
+      <div className='grid'>
         {posts.map(post => {
           return (
             <Link href={`/posts/[id]`} as={`/posts/${post.id}`} key={post.id}>
-              <a>
+              <a className='card'>
                 <h3>{post.title}</h3>
                 <p>{post.body}</p>
               </a>
@@ -30,6 +31,52 @@ export default function Posts() {
           )
         })}
       </div>
+      <style jsx>
+        {`
+          .grid {
+            display: flex;
+            flex-wrap: wrap;
+            max-width: 800px;
+            margin-top: 2rem;
+          }
+          .card {
+            margin: 1rem;
+            flex-basis: 45%;
+            padding: 1.5rem;
+            color: black;
+            text-decoration: none;
+            border: 2px solid #eaeaea;
+            border-radius: 10px;
+            transition: color 0.15s ease, border-color 0.15s ease;
+          }
+          .card:hover,
+          .card:focus,
+          .card:active {
+            color: #0070f3;
+            border-color: #0070f3;
+          }
+          .card h3 {
+            margin: 0 0 1rem 0;
+            font-size: 1.5rem;
+          }
+          .card p {
+            margin: 0;
+            font-size: 1.25rem;
+            line-height: 1.5;
+          }
+        `}
+      </style>
     </Layout>
   )
+}
+
+export async function getServerSideProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await res.json();
+
+  return {
+    props: {
+      posts
+    }
+  }
 }
