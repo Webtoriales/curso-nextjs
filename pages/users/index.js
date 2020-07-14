13b66e3cd4af5cd2a87c7c58aef72119d459e000
Andreas <andreaslorozco@gmail.com>
2020-07-13 18:25:42 -0600

    adds css for lesson 12

--- pages/users/index.js
+++ pages/users/index.js
@@ -2,73 +2,77 @@ import Link from 'next/link';
 import Title from '../../components/title';
 import Layout from '../../components/layout';
 
-export default function Users({ users }) {
+export default function User({ user }) {
+  const router = useRouter()
+
   return(
     <Layout>
-      <Title>Users Page</Title>
-      <div className='grid'>
-        {users.map(user => {
-          return (
-            <Link href={'/users/[id]'} as={`/users/${user.id}`} key={user.id}>
-              <a className='card'>
-                <h3>User</h3>
-                <p>Name: {user.name}</p>
-                <p>Email: {user.email}</p>
-              </a>
-            </Link>
-          )
-        })}
+      <Title>
+        User ID {user.id}
+      </Title>
+      <div className="card">
+        <h3>User</h3>
+        <p>Name: {user.name}</p>
+        <p>Email: {user.email}</p>
+        <p>Phone: {user.phone}</p>
+        <p>Website: {user.name}</p>
       </div>
+
       <style jsx>
-      {`
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-      `}
+        {`
+          .card {
+            margin: 1rem;
+            flex-basis: 45%;
+            padding: 1.5rem;
+            text-align: left;
+            color: inherit;
+            text-decoration: none;
+            border: 1px solid #eaeaea;
+            border-radius: 10px;
+            transition: color 0.15s ease, border-color 0.15s ease;
+          }
+  
+          .card:hover,
+          .card:focus,
+          .card:active {
+            color: #0070f3;
+            border-color: #0070f3;
+          }
+  
+          .card h3 {
+            margin: 0 0 1rem 0;
+            font-size: 1.5rem;
+          }
+  
+          .card p {
+            margin: 0;
+            font-size: 1.25rem;
+            line-height: 1.5;
+          }
+        `}
       </style>
     </Layout>
   )
 }
 
-export async function getStaticProps() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
-  const users = await res.json();
+export async function getStaticPaths() {
+  const paths = [
+    {params: { id: '1' }},
+  ];
 
   return {
-    props: {
-      users
-    }
+    paths,
+    fallback: false
   }
 }
 
+export async function getStaticProps({ params }) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`);
+  const user = await res.json();
+
+  return {
+    props: {
+      user
+    }
+  }
+}
