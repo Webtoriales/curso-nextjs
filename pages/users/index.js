1d3281fc87621fdd8c3fe81c7653f8e082d9d5d3
Andreas <andreaslorozco@gmail.com>
2020-07-13 19:03:41 -0600

    fixes incorrect merge

--- pages/users/index.js
+++ pages/users/index.js
@@ -2,77 +2,72 @@ import Link from 'next/link';
 import Title from '../../components/title';
 import Layout from '../../components/layout';
 
-export default function User({ user }) {
-  const router = useRouter()
-
+export default function Users({ users }) {
   return(
     <Layout>
-      <Title>
-        User ID {user.id}
-      </Title>
-      <div className="card">
-        <h3>User</h3>
-        <p>Name: {user.name}</p>
-        <p>Email: {user.email}</p>
-        <p>Phone: {user.phone}</p>
-        <p>Website: {user.name}</p>
+      <Title>Users Page</Title>
+      <div className='grid'>
+        {users.map(user => {
+          return (
+            <Link href={'/users/[id]'} as={`/users/${user.id}`} key={user.id}>
+              <a className='card'>
+                <h3>User</h3>
+                <p>Name: {user.name}</p>
+                <p>Email: {user.email}</p>
+              </a>
+            </Link>
+          )
+        })}
       </div>
-
       <style jsx>
-        {`
-          .card {
-            margin: 1rem;
-            flex-basis: 45%;
-            padding: 1.5rem;
-            text-align: left;
-            color: inherit;
-            text-decoration: none;
-            border: 1px solid #eaeaea;
-            border-radius: 10px;
-            transition: color 0.15s ease, border-color 0.15s ease;
-          }
-  
-          .card:hover,
-          .card:focus,
-          .card:active {
-            color: #0070f3;
-            border-color: #0070f3;
-          }
-  
-          .card h3 {
-            margin: 0 0 1rem 0;
-            font-size: 1.5rem;
-          }
-  
-          .card p {
-            margin: 0;
-            font-size: 1.25rem;
-            line-height: 1.5;
-          }
-        `}
+      {`
+        .grid {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-wrap: wrap;
+          max-width: 800px;
+          margin-top: 3rem;
+        }
+        .card {
+          margin: 1rem;
+          flex-basis: 45%;
+          padding: 1.5rem;
+          text-align: left;
+          color: inherit;
+          text-decoration: none;
+          border: 1px solid #eaeaea;
+          border-radius: 10px;
+          transition: color 0.15s ease, border-color 0.15s ease;
+        }
+        .card:hover,
+        .card:focus,
+        .card:active {
+          color: #0070f3;
+          border-color: #0070f3;
+        }
+        .card h3 {
+          margin: 0 0 1rem 0;
+          font-size: 1.5rem;
+        }
+        .card p {
+          margin: 0;
+          font-size: 1.25rem;
+          line-height: 1.5;
+        }
+      `}
       </style>
     </Layout>
   )
 }
 
-export async function getStaticPaths() {
-  const paths = [
-    {params: { id: '1' }},
-  ];
-
-  return {
-    paths,
-    fallback: false
-  }
-}
-
-export async function getStaticProps({ params }) {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`);
-  const user = await res.json();
+export async function getStaticProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users = await res.json();
 
   return {
     props: {
-      user
+      users
     }
   }
 }
